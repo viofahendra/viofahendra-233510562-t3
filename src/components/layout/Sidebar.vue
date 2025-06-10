ac7f65e5011fcc37602d52c9387e4b4764f0062a
viofahendra <viofahendra@student.uir.ac.id>
2025-06-11 02:13:34 +0700

    Update Sidebar.vue

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -9,20 +9,20 @@ const navItems = [
 </script>
 
 <template>
-  <nav class="w-54 fixed t-0 h-screen bg-secondary text-gray-800 shadow-xl px-6 py-8 font-sans">
+  <nav class="w-54 fixed top-0 h-screen bg-secondary text-gray-800 shadow-xl px-6 py-8 font-sans">
     <h2 class="text-2xl font-bold mb-6 tracking-wide border-b pb-2 text-gray-900">
       Pertemuan 4
     </h2>
 
     <ul class="space-y-4 text-lg">
       <li v-for="item in navItems" :key="item.href">
-        <a
-          :href="item.href"
+        <router-link
+          :to="item.href"
           class="block px-4 py-2 rounded-md transition duration-200 ease-in-out 
                  hover:bg-white hover:text-secondary hover:shadow-sm font-medium"
         >
           {{ item.name }}
-        </a>
+        </router-link>
       </li>
     </ul>
   </nav>
